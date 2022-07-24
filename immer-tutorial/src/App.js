d88350a27e2931c9001836078d8f53b5167d0e85
박현준 <78728441+guswns0530@users.noreply.github.com>
2022-07-24 15:24:25 +0900

    redux

--- immer-tutorial/src/App.js
+++ immer-tutorial/src/App.js
@@ -1,78 +1,72 @@
-import { useCallback, useRef, useState } from 'react';
-import produce from 'immer'
+import { useCallback, useRef, useState } from "react";
+import produce from "immer";
 
 function App() {
-  const nextId = useRef(1)
+  const nextId = useRef(1);
   const [form, setForm] = useState({
-    name: '',
-    username: ''
+    name: "",
+    username: "",
   });
   const [data, setData] = useState({
     array: [],
-    uselessValue: null
-  })
+  });
 
-  const onChange = useCallback(
-    e => {
-      const { name, value } = e.target
-      // setForm(
-      //   produce(draft => {
-      //     draft[name] = value;
-      //   })
-      // )
+  const onChange = useCallback((e) => {
+    const { name, value } = e.target;
+    // setForm(
+    //   produce(draft => {
+    //     draft[name] = value;
+    //   })
+    // )
 
-      setForm(
-        produce(form => {
-          form[name] = value
-        })
-      )
-    },
-    []
-  )
+    setForm(
+      produce((form) => {
+        form[name] = value;
+      })
+    );
+  }, []);
 
   const onSubmit = useCallback(
-    e => {
+    (e) => {
       e.preventDefault();
       const info = {
         id: nextId.current,
         name: form.name,
-        username: form.username
-      }
+        username: form.username,
+      };
 
       setData(
-        produce(draft => {
-          draft.array.push(info)
+        produce((draft) => {
+          draft.array.push(info);
         })
-      )
-
-      console.log(data)
+      );
 
       setForm({
-        name: '',
-        username: ''
-      })
+        name: "",
+        username: "",
+      });
       nextId.current += 1;
     },
     [form.name, form.username]
-  )
+  );
 
-  const onRemove = useCallback(
-    id => {
-      setData(
-        produce(draft => {
-          draft.array.splice(draft.array.findIndex(info => info.id === id), 1)
-        })
-      )
-    },
-    []
-  )
+  const onRemove = useCallback((id) => {
+    setData(
+      produce((draft) => {
+        draft.array.splice(
+          draft.array.findIndex((info) => info.id === id),
+          1
+        );
+      })
+    );
+  }, []);
 
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input 
+        <input
           name="username"
-          placeholder='아이디'
+          placeholder="아이디"
           value={form.username}
           onChange={onChange}
         />
@@ -82,11 +76,11 @@ function App() {
           value={form.name}
           onChange={onChange}
         />
-        <button type='submit'>등록</button>
+        <button type="submit">등록</button>
       </form>
       <div>
         <ul>
-          {data.array.map(info => (
+          {data.array.map((info) => (
             <li key={info.id} onClick={() => onRemove(info.id)}>
               {info.username} {info.name}
             </li>
@@ -96,6 +90,5 @@ function App() {
     </div>
   );
 }
-
 
 export default App;
