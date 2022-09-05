747aea9ccd62ad1a5f3344d8986d77e76744696b
박현준 <wooae1234@gmail.com>
2022-09-05 14:40:09 +0900

    ~

--- learn-redux-middleware/src/lib/api.js
+++ learn-redux-middleware/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const getPost = (id) => {
+export const  getPost = (id) => {
   return axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
 };
 export const getUsers = (id) => {
